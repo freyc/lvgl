777cbae46dcebf5317b2d1d5ab2b5009f0ad428b
Christian Frey <christian.frey@embedded-insights.ch>
2021-10-06 17:26:19 +0200

    added examples to lvgl-cmake-file

--- sources.cmake
+++ sources.cmake
@@ -136,3 +136,74 @@ src/gpu/sdl/lv_gpu_sdl_utils.c
 src/hal/lv_hal_disp.c
 src/hal/lv_hal_indev.c
 src/hal/lv_hal_tick.c)
+
+set(EXAMPLE_SOURCES
+examples/anim/lv_example_anim_1.c                 examples/widgets/bar/lv_example_bar_4.c
+examples/anim/lv_example_anim_2.c                 examples/widgets/bar/lv_example_bar_5.c
+examples/anim/lv_example_anim_3.c                 examples/widgets/bar/lv_example_bar_6.c
+examples/anim/lv_example_anim_timeline_1.c        examples/widgets/btn/lv_example_btn_1.c
+examples/assets/animimg001.c                      examples/widgets/btn/lv_example_btn_2.c
+examples/assets/animimg002.c                      examples/widgets/btn/lv_example_btn_3.c
+examples/assets/animimg003.c                      examples/widgets/btnmatrix/lv_example_btnmatrix_1.c
+examples/assets/imgbtn_left.c                     examples/widgets/btnmatrix/lv_example_btnmatrix_2.c
+examples/assets/imgbtn_mid.c                      examples/widgets/btnmatrix/lv_example_btnmatrix_3.c
+examples/assets/imgbtn_right.c                    examples/widgets/calendar/lv_example_calendar_1.c
+examples/assets/img_caret_down.c                  examples/widgets/canvas/lv_example_canvas_1.c
+examples/assets/img_cogwheel_alpha16.c            examples/widgets/canvas/lv_example_canvas_2.c
+examples/assets/img_cogwheel_argb.c               examples/widgets/chart/lv_example_chart_1.c
+examples/assets/img_cogwheel_chroma_keyed.c       examples/widgets/chart/lv_example_chart_2.c
+examples/assets/img_cogwheel_indexed16.c          examples/widgets/chart/lv_example_chart_3.c
+examples/assets/img_cogwheel_rgb.c                examples/widgets/chart/lv_example_chart_4.c
+examples/assets/img_hand.c                        examples/widgets/chart/lv_example_chart_5.c
+examples/assets/img_skew_strip.c                  examples/widgets/chart/lv_example_chart_6.c
+examples/assets/img_star.c                        examples/widgets/chart/lv_example_chart_7.c
+examples/event/lv_example_event_1.c               examples/widgets/chart/lv_example_chart_8.c
+examples/event/lv_example_event_2.c               examples/widgets/chart/lv_example_chart_9.c
+examples/event/lv_example_event_3.c               examples/widgets/checkbox/lv_example_checkbox_1.c
+examples/get_started/lv_example_get_started_1.c   examples/widgets/checkbox/lv_example_checkbox_2.c
+examples/get_started/lv_example_get_started_2.c   examples/widgets/colorwheel/lv_example_colorwheel_1.c
+examples/get_started/lv_example_get_started_3.c   examples/widgets/dropdown/lv_example_dropdown_1.c
+examples/layouts/flex/lv_example_flex_1.c         examples/widgets/dropdown/lv_example_dropdown_2.c
+examples/layouts/flex/lv_example_flex_2.c         examples/widgets/dropdown/lv_example_dropdown_3.c
+examples/layouts/flex/lv_example_flex_3.c         examples/widgets/imgbtn/lv_example_imgbtn_1.c
+examples/layouts/flex/lv_example_flex_4.c         examples/widgets/img/lv_example_img_1.c
+examples/layouts/flex/lv_example_flex_5.c         examples/widgets/img/lv_example_img_2.c
+examples/layouts/flex/lv_example_flex_6.c         examples/widgets/img/lv_example_img_3.c
+examples/layouts/grid/lv_example_grid_1.c         examples/widgets/img/lv_example_img_4.c
+examples/layouts/grid/lv_example_grid_2.c         examples/widgets/keyboard/lv_example_keyboard_1.c
+examples/layouts/grid/lv_example_grid_3.c         examples/widgets/label/lv_example_label_1.c
+examples/layouts/grid/lv_example_grid_4.c         examples/widgets/label/lv_example_label_2.c
+examples/layouts/grid/lv_example_grid_5.c         examples/widgets/label/lv_example_label_3.c
+examples/layouts/grid/lv_example_grid_6.c         examples/widgets/led/lv_example_led_1.c
+examples/others/snapshot/lv_example_snapshot_1.c  examples/widgets/line/lv_example_line_1.c
+examples/porting/lv_port_disp_template.c          examples/widgets/list/lv_example_list_1.c
+examples/porting/lv_port_fs_template.c            examples/widgets/list/lv_example_list_2.c
+examples/porting/lv_port_indev_template.c         examples/widgets/meter/lv_example_meter_1.c
+examples/scroll/lv_example_scroll_1.c             examples/widgets/meter/lv_example_meter_2.c
+examples/scroll/lv_example_scroll_2.c             examples/widgets/meter/lv_example_meter_3.c
+examples/scroll/lv_example_scroll_3.c             examples/widgets/meter/lv_example_meter_4.c
+examples/scroll/lv_example_scroll_4.c             examples/widgets/msgbox/lv_example_msgbox_1.c
+examples/scroll/lv_example_scroll_5.c             examples/widgets/obj/lv_example_obj_1.c
+examples/scroll/lv_example_scroll_6.c             examples/widgets/obj/lv_example_obj_2.c
+examples/styles/lv_example_style_10.c             examples/widgets/roller/lv_example_roller_1.c
+examples/styles/lv_example_style_11.c             examples/widgets/roller/lv_example_roller_2.c
+examples/styles/lv_example_style_12.c             examples/widgets/roller/lv_example_roller_3.c
+examples/styles/lv_example_style_13.c             examples/widgets/slider/lv_example_slider_1.c
+examples/styles/lv_example_style_14.c             examples/widgets/slider/lv_example_slider_2.c
+examples/styles/lv_example_style_1.c              examples/widgets/slider/lv_example_slider_3.c
+examples/styles/lv_example_style_2.c              examples/widgets/span/lv_example_span_1.c
+examples/styles/lv_example_style_3.c              examples/widgets/spinbox/lv_example_spinbox_1.c
+examples/styles/lv_example_style_4.c              examples/widgets/spinner/lv_example_spinner_1.c
+examples/styles/lv_example_style_5.c              examples/widgets/switch/lv_example_switch_1.c
+examples/styles/lv_example_style_6.c              examples/widgets/table/lv_example_table_1.c
+examples/styles/lv_example_style_7.c              examples/widgets/table/lv_example_table_2.c
+examples/styles/lv_example_style_8.c              examples/widgets/tabview/lv_example_tabview_1.c
+examples/styles/lv_example_style_9.c              examples/widgets/tabview/lv_example_tabview_2.c
+examples/widgets/animimg/lv_example_animimg_1.c   examples/widgets/textarea/lv_example_textarea_1.c
+examples/widgets/arc/lv_example_arc_1.c           examples/widgets/textarea/lv_example_textarea_2.c
+examples/widgets/arc/lv_example_arc_2.c           examples/widgets/textarea/lv_example_textarea_3.c
+examples/widgets/bar/lv_example_bar_1.c           examples/widgets/tileview/lv_example_tileview_1.c
+examples/widgets/bar/lv_example_bar_2.c           examples/widgets/win/lv_example_win_1.c
+examples/widgets/bar/lv_example_bar_3.c
+
+)
